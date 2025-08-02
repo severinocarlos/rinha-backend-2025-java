21d489245dae611f5e0546e9387a6854119d8012
Severino Carlos <severinocsilva3@gmail.com>
2025-08-02 00:08:17 -0300

    chore: init structure

---
+++ src/main/java/com/severinocarlos/rinha/RinhaDeBackend2025Application.java
@@ -0,0 +1,13 @@
+package com.severinocarlos.rinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaDeBackend2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaDeBackend2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/severinocarlos/rinha/RinhaDeBackend2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severinocarlos.rinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaDeBackend2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
